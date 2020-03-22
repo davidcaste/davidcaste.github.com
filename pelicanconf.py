a06b4a952f50a4cac02b1db6d9f5dc933e9418b4
David Castellanos <davidcaste@gmail.com>
2020-03-23 00:32:49 +0100

    Change SITEURL to https://www.pataliebre.net

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'David Castellanos'
 SITENAME = 'pataliebre.net'
-SITEURL = 'https://pataliebre.net'
+SITEURL = 'https://www.pataliebre.net'
 
 PATH = 'content'
 
